f062b035959118d08f48796b1f5a166bed868515
cycy <1531859775@qq.com>
2023-11-22 14:57:37 +0800

    feat: 多组件共享数据

---
+++ src/component/Person.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h3>上方count组件的求和为：{{sum}}</h3>
+    <h3>当前人员列表</h3>
+    <input type="text" placeholder="please input name" v-model="name">
+    <button @click="add">addPersonName</button>
+    <ul>
+        <li v-for="p in personList" :key="p.id">{{p.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { nanoid } from 'nanoid';
+import {mapState} from 'vuex'
+export default {
+  name:'Person',
+  data() {
+    return {
+      name:''
+    }
+  },
+  methods: {
+    add(){
+      const personObj = {id:nanoid(),name:this.name}
+      console.log('personObj :>> ', personObj);
+      this.$store.commit('ADD_PERSON',personObj)
+      this.name = ''
+    },
+  },
+  computed:{
+    sum(){
+      return this.$store.state.sum
+    },
+    personList(){
+      return this.$store.state.personList
+    },
+  // ...mapState(['personList'])
+  }
+}
+</script>
+
+<style>
+
+</style>
