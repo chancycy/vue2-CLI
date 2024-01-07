0c672dd37d4314f20617b81eb32d74d0952efeef
cycy <1531859775@qq.com>
2024-01-07 22:43:40 +0800

    feat: 路由的query参数和命名路由name

---
+++ 18_src_router-query&name/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>this is About</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'About',
+    mounted() {
+      console.log('about组件挂载完毕, this :>> ', this);
+      window.aboutRoute = this.$route
+      window.aboutRouter = this.$router
+    },
+}
+</script>
+
